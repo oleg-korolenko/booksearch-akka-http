9c20961d5e0b8b8c5e3e3d76c76f48f4d0489fe9
Oleg Korolenko <oleg.korolenko@gmail.com>
2016-06-01 22:30:52 +0200

    Added some docs

--- src/main/scala/com/wemanity/booksearch/actor/ActorHttpClient.scala
+++ src/main/scala/com/wemanity/booksearch/actor/ActorHttpClient.scala
@@ -7,6 +7,7 @@ import akka.stream.ActorMaterializer
 import scala.concurrent.Future
 
 /**
+  * Trait mo mix with actors, provides some basic Http operations
   * Created by oleg on 09/03/16.
   */
 trait ActorHttpClient {
@@ -19,13 +20,5 @@ trait ActorHttpClient {
 
   def get(url: String):Future[HttpResponse] = {
     http.singleRequest(HttpRequest(uri = url))
-    /*
-    Http().singleRequest(RequestBuilding.Get(uri)).map { response =>
-      response.status match {
-        case OK       => Right(response.entity.dataBytes.via(csv.parse())
-        case NotFound => Left(NotFound -> s"No data found for $symbol")
-        case status   => Left(status -> s"Request to $uri failed with status $status")
-      }
-    }*/
   }
 }
